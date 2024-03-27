a8ea0940341152dea87fcb05ac55afbdd37c1165
rvdang256 <rvd5465@psu.edu>
2024-03-27 11:55:03 -0400

    removed the logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,4 @@
 
-import LogoTitle from '../../assets/images/logo-s.png';
 import Resume from '../../assets/resume.pdf';
 import { useState, useEffect } from 'react';
 import './index.scss';
@@ -9,7 +8,7 @@ import Loader from 'react-loaders';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['l', 'y', 'a', 'n']
+    const nameArray = ['I',"'m",'', '',  'R','y', 'a', 'n']
     const jobArray = ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
     useEffect(() => {
          setTimeout(() => {
@@ -27,10 +26,8 @@ const Home = () => {
                 <span className={letterClass}>H</span>
                 <span className={`${letterClass} _12`}>i,</span>
                 <br />
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
-                <img src={LogoTitle} alt="developer" />
-                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+                {/* <img src={LogoTitle} alt="developer" /> */}
+                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={13} />
                 <br />
                 <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={18} />
                 </h1>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,17 +1,13 @@
 import'./index.scss';
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
-import { Link, NavLink } from 'react-router-dom';
+
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className="nav-bar">
-        <Link className= "logo" to = '/'>
-            <img src={LogoS} alt="logo"/>
-            <img className = "sub-logo" src={LogoSubtitle} alt="Ryan"/>
-        </Link>
+        
         <nav>
 
             <NavLink exact = 'true' activeClassName = "active" to = '/'>
