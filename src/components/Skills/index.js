06d151a8740b6c6f6c06fefe2085a264eb150b92
rvdang256 <rvd5465@psu.edu>
2024-05-07 11:42:17 -0400

    fixed grammar issues in skill page

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -34,13 +34,13 @@ const Skills = () => {
             
           I have worked as an IT intern at the Applied Research Laboratory at Penn State where I was responsible for managing
           the IT infrastructure and providing technical support to the research staff. This includes handling maintence of the
-          the different Atlassain tools, creating new macros JIRA and Confluence, and updating the knowledge base.
+          the different Atlassain tools, creating new macros in JIRA and Confluence, and updating the knowledge base.
           
           </p>
           <p>
             I have created many side projects in my free time to learn new technologies and to improve my skills. Some of these projects
-            are a <a href='https://rhythmic-rights.vercel.app/' target='_blank' rel='noreferrer'>Audio Minting NFT</a> website, an 
-             <a href='https://github.com/rvdang256/Article-Of-Clothing-Classifier' target='_blank' rel='noreferrer'>Article of Clothing Classifier</a> , 
+            are a <a href='https://rhythmic-rights.vercel.app/' target='_blank' rel='noreferrer'>Audio Minting NFT</a> website, 
+            an <a href='https://github.com/rvdang256/Article-Of-Clothing-Classifier' target='_blank' rel='noreferrer'>Article of Clothing Classifier</a>, 
             and a <a href='https://fitnessjourney-3h4rcrtye-ryan-dangs-projects.vercel.app/' target='_blank' rel='noreferrer'>Calorie Tracker</a> website. To see more of my projects, please
             visit my <a href='https://github.com/rvdang256' target='_blank' rel='noreferrer'>GitHub</a> page.
           </p>
